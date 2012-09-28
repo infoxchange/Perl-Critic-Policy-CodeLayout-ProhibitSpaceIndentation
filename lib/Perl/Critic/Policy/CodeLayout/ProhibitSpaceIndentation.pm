503013268f24c344437151a9d599e8ad0966f091
Alexey Kotlyarov <alexey@infoxchange.net.au>
2012-09-28 16:29:17 +1000

    Vanilla module-starter template.

---
+++ lib/Perl/Critic/Policy/CodeLayout/ProhibitSpaceIndentation.pm
@@ -0,0 +1,111 @@
+package Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation - The great new Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation;
+
+    my $foo = Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Infoxchange Australia, C<< <devs at infoxchange.net.au> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-perl-critic-policy-codelayout-prohibitspaceindentation at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Infoxchange Australia.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation
