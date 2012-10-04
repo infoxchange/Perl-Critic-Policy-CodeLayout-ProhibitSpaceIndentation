451bff4a8cd5173fbcc68d9d928a27cafba5db8e
Alexey Kotlyarov <alexey@infoxchange.net.au>
2012-10-04 11:29:26 +1000

    Put the actual module in.

--- lib/Perl/Critic/Policy/CodeLayout/ProhibitSpaceIndentation.pm
+++ lib/Perl/Critic/Policy/CodeLayout/ProhibitSpaceIndentation.pm
@@ -1,111 +1,99 @@
 package Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation;
 
-use 5.006;
 use strict;
 use warnings;
+use Readonly;
 
+our $VERSION = '1.00';
+
+use Perl::Critic::Utils qw{ :booleans :severities };
+use base qw(Perl::Critic::Policy);
+
+Readonly::Scalar my $DESC => q{Spaces used for indentation};
+
+# Match possibly some blank lines, then indentation with at least one space
+# (possibly among tabs).
+Readonly::Scalar my $SPACE_INDENT_REGEX => qr/^(\n*)\t* +\t*/;
+
+sub default_severity {
+	return $SEVERITY_LOW;
+}
+
+sub default_themes {
+	return qw(cosmetic);
+}
+
+sub applies_to {
+	return 'PPI::Token';
+}
+
+sub violates {
+	my ($self, $elem, undef) = @_;
+
+	# Only a violation at line start
+	if ($elem->location->[1] == 1 && $elem =~ $SPACE_INDENT_REGEX) {
+		return $self->violation($DESC, undef, $elem);
+	} else {
+		return;
+	}
+}
+
+1;
+__END__
 =head1 NAME
 
-Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation - The great new Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
+Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation - Use tabs instead
+of spaces for indentation.
 
 
-=head1 SYNOPSIS
+=head1 DESCRIPTION
 
-Quick summary of what the module does.
 
-Perhaps a little code snippet.
+For projects which have a policy of using tabs for indentation, you want to
+ensure there are no spaces used for that purpose. This Policy catches all
+such occurrences so that you can be sure when the tab sizes are reconfigured,
+spaces won't make indented code look wrong.
 
-    use Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation;
+This policy can be used together with
+L<Perl::Critic::Policy::CodeLayout::ProhibitHardTabs|CodeLayout::ProhibitHardTabs>
+by setting C<allow_leading_tabs> option of the latter to C<1>.
 
-    my $foo = Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation->new();
-    ...
+Putting hard tabs in your source code (or POD) is one of the worst
+things you can do to your co-workers and colleagues, especially if
+those tabs are anywhere other than a leading position.  Because
+various applications and devices represent tabs differently, they can
+cause you code to look vastly different to other people.  Any decent
+editor can be configured to expand tabs into spaces.
+L<Perl::Tidy|Perl::Tidy> also does this for you.
 
-=head1 EXPORT
+This Policy catches all tabs in your source code, including POD,
+quotes, and HEREDOCs.  The contents of the C<__DATA__> section are not
+examined.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
 
-=head1 SUBROUTINES/METHODS
+=head1 CONFIGURATION
 
-=head2 function1
+This Policy is not configurable except for the standard options.
 
-=cut
 
-sub function1 {
-}
+=head1 NOTES
 
-=head2 function2
+If there are blank lines before a violating line, the first blank line will be
+reported as the violation location, because all the whitespace forms a single
+token which Perl::Critic gives to the policy.
 
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
-Infoxchange Australia, C<< <devs at infoxchange.net.au> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-perl-critic-policy-codelayout-prohibitspaceindentation at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Infoxchange Australia <devs@infoxchange.net.au>
 
 
+=head1 COPYRIGHT
 
+Copyright (c) 2012 Infoxchange Australia.  All rights reserved.
 
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Perl-Critic-Policy-CodeLayout-ProhibitSpaceIndentation/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2012 Infoxchange Australia.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.  The full text of this license
+can be found in the LICENSE file included with this module.
 
 =cut
-
-1; # End of Perl::Critic::Policy::CodeLayout::ProhibitSpaceIndentation
